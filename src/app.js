2580dbe75b65a602a43c808ff1a8684f7a72f828
nikavella <nikavella22@gmail.com>
2023-05-28 21:05:23 +0600

    added morgan logging

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 const planetsRouter = require('./routes/planets/planets.router.js')
 
 const app = express();
+
 app.use(cors({
   origin: 'http://localhost:3000'
 }))
+app.use(morgan('combined'));
 app.use(express.json());
+
 app.use(planetsRouter);
 
 module.exports = app;
