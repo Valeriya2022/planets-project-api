e11a4de8d78aa3a78e041bb1a970b317e2dd85b4
nikavella <nikavella22@gmail.com>
2023-05-28 20:40:17 +0600

    load data asyncronuasly before starting the server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 const http = require('http');
 const app = require('./app.js');
 const PORT = process.env.PORT || 8000;
+const { loadPlanetsData } = require('../src/models/planets.model');
 
 const server = http.createServer(app);
 
-server.listen(PORT, ()=>{
-  console.log(`Listening on port ${PORT}...`)
-})
+async function startServer (){
+  await loadPlanetsData();
+  server.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}...`)
+  })
+}
+
+startServer();
+
+
 
 
