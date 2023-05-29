524cac8db493d622934aa08ed95f23f7287cd393
nikavella <nikavella22@gmail.com>
2023-05-29 16:58:09 +0600

    added launches model, controller and router

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const planetsRouter = require('./routes/planets/planets.router.js')
+const launchesRouter = require('./routes/launches/launches.router.js')
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(morgan('combined'));
 app.use(express.json());
 
 app.use(planetsRouter);
+app.use(launchesRouter);
 
 module.exports = app;
