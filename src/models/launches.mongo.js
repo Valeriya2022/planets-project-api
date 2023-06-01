b3d1723bf6dccdbfd1e74a6a96070d18928e37a0
nikavella <nikavella22@gmail.com>
2023-06-01 17:32:00 +0600

    created launches schema

---
+++ src/models/launches.mongo.js
@@ -0,0 +1,31 @@
+ const mongoose = require('mongoose');
+
+ const launchSchema = new  mongoose.Schema({
+   flightNumber: {
+     type: Number,
+     required: true
+   },
+   launchDate: {
+     type: Date,
+     required: true
+   },
+   mission: {
+     type: String,
+     required: true
+   },
+   rocket: {
+     type: String,
+     required: true
+   },
+   upcoming: {
+     type: Boolean ,
+     required: true
+   },
+   success: {
+     type: Boolean,
+     required: true,
+     default: true
+   },
+   customer: [ String ]
+
+ })
