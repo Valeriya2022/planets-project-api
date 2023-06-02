0b4e91bbe2f33419b5816cc2cd35e2e027419565
nikavella <nikavella22@gmail.com>
2023-06-02 21:10:24 +0600

    move mongo related code from server.js to services/mongo.js file

--- src/server.js
+++ src/server.js
@@ -1,30 +1,15 @@
 const http = require('http');
-const mongoose = require('mongoose');
 require('dotenv').config()
-
 const app = require('./app.js');
-
 const { loadPlanetsData } = require('../src/models/planets.model');
+const { mongoConnect } = require("./services/mongo");
 
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = process.env.MONGO_URL;
-
-mongoose.connection.once('open', ()=>{
-  console.log('MongoDB connection ready!')
-})
-
-mongoose.connection.on('error', (err)=>{
-  console.error(err);
-})
-
 async function startServer (){
-  await  mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoConnect();
   await loadPlanetsData();
   server.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}...`)
